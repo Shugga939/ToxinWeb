b4138eb63babe753593c108c04c0483fae050920
Shugga939 <Shugga939@gmail.com>
2021-05-04 20:58:51 +0300

    First commit

---
+++ public/main.js
@@ -0,0 +1,49 @@
+import './main.scss';
+var datepicker = $('#startdate').datepicker().data('datepicker');
+var startdate = $('#startdate');
+
+$(document).ready(function(){
+
+$('#startdate').datepicker({
+    range: true,
+    todayButton: true,
+    clearButton: true,
+    
+    
+})
+    
+$('#startdate').datepicker({
+    
+    onSelect: function(fd){
+        $('#startdate').val(fd.split(",")[0]);
+        $('#enddate').val(fd.split(",")[1]);
+        
+    }
+
+})
+})
+$("#butt").click( function (){
+    if(startdate.hasClass('shower')) {
+        startdate.removeClass('shower');
+        datepicker.hide()}
+    else {startdate.addClass('shower');
+        datepicker.show()}
+})
+
+$("#butt2").click( function (){
+    if(startdate.hasClass('shower')) {
+        startdate.removeClass('shower');
+        datepicker.hide()}
+    else {startdate.addClass('shower');
+        datepicker.show()}
+})
+
+
+$(document).click(function(event){
+    if (startdate.hasClass('shower')){
+    startdate.removeClass('shower')}
+})
+/*
+startdate.click(function (){
+    datepicker.hide();
+})*/
