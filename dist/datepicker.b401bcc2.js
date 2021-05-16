8cee136d608788e9a6b93c341ac4086152ea986d
Shugga939 <Shugga939@gmail.com>
2021-05-16 16:40:25 +0300

    add footer

--- dist/datepicker.b401bcc2.js
+++ dist/datepicker.b401bcc2.js
@@ -2324,7 +2324,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58549" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52292" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
