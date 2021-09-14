4a1f40c7d84c54031f27cfa0272b5dd4e4acf19f
Shugga939 <Shugga939@email.com>
2021-09-14 20:28:18 +0300

    work this ui kit (cards_2)

---
+++ UIKit/Cards/Cards.js
@@ -0,0 +1,11 @@
+window.$ = require('jquery')
+window.jQuery = require('jquery')
+
+$(document).ready(function(){
+  // alert('s')
+ $('.room-cart-slick').slick({
+  dots: true,
+  accessibility: true,
+ });
+
+})
