6289dfd0bd3c237d66643c10bae8da93a42c87ee
Shugga939 <Shugga939@email.com>
2021-09-22 21:04:54 +0300

    edit js, drop menu

--- public/js/datepickerLogic.js
+++ public/js/datepickerLogic.js
@@ -1,83 +1,103 @@
 export const initDatePicker = function (){
-var datepicker = $('#arrival').datepicker().data('datepicker');
-//inputs
-var firstdate = $('#arrival');
-var lastdate = $('#leaving');
-//buttons for inputs
-var firstdate_button = $("#button_arrival");
-var lastdate_button = $("#button_leaving");
+  let firstdate = $('input[name="dateArrival"]');
+  let lastdate = $('input[name="dateLeaving"]');
+  let firstdate_button = firstdate.siblings("button");
+  let lastdate_button = lastdate.siblings("button");
+  let datepicker = firstdate.datepicker().data('datepicker');
 
-firstdate.datepicker({
-  range: true,
-  todayButton: true,
-  clearButton: true,
-  minDate: new Date(),
-  toggleSelected: true,
-  
-  onSelect: function(fd){
-    firstdate.val(fd.split(",")[0]);
-    lastdate.val(fd.split(",")[1]);
-  }
-})
 
-firstdate.on('click', function(){
-  firstdate_button.trigger('click');
-  
-})
-lastdate.on('click', function(){
-  lastdate_button.trigger('click');
-})
-
-firstdate_button.on('click', function (){
-  if(firstdate.hasClass('show_datepicker')) {
-    datepicker.hide();
-    lastdate_button.attr('disabled',false)
-    lastdate.attr('disabled',false)
-    firstdate.toggleClass('show_datepicker');
-    $(this).removeClass('dropbutton_open')
-    $(this).addClass('dropbutton_close')
+  if (lastdate.length && firstdate.length){
+    firstdate.datepicker({
+    range: true,
+    todayButton: true,
+    clearButton: true,
+    minDate: new Date(),
+    toggleSelected: true,
+    
+    onSelect: function(date){
+      firstdate.val(date.split(",")[0]);
+      lastdate.val(date.split(",")[1]);
+    }})
   } else {
-    datepicker.show();
-    lastdate_button.attr('disabled',true)
-    lastdate.attr('disabled',true)
-    firstdate.toggleClass('show_datepicker');
-    $(this).addClass('dropbutton_open')
-    $(this).removeClass('dropbutton_close')
-  }
-})
+    firstdate.datepicker({
+      range: true,
+      todayButton: true,
+      clearButton: true,
+      minDate: new Date(),
+      toggleSelected: true,
+      
+    //do It
+  })}
+  
+  firstdate_button.on('click', function (){
+    if(firstdate.hasClass('show_datepicker')) {
+      datepicker.hide();
+      lastdate_button.attr('disabled',false)
+      lastdate.attr('disabled',false)
+      firstdate.toggleClass('show_datepicker');
+      $(this).removeClass('dropbutton_open')
+      $(this).addClass('dropbutton_close')
+    } else {
+      datepicker.show();
+      lastdate_button.attr('disabled',true)
+      lastdate.attr('disabled',true)
+      firstdate.toggleClass('show_datepicker');
+      $(this).addClass('dropbutton_open')
+      $(this).removeClass('dropbutton_close')
+    }
+  })
+  
+  lastdate_button.on('click', function (){
+    if(lastdate.hasClass('show_datepicker')) {
+      datepicker.hide();
+      firstdate_button.attr('disabled',false)
+      firstdate.attr('disabled',false)
+      lastdate.toggleClass('show_datepicker');
+      $(this).removeClass('dropbutton_open')
+      $(this).addClass('dropbutton_close')
+    } else {
+      datepicker.show();
+      firstdate_button.attr('disabled',true)
+      firstdate.attr('disabled',true)
+      lastdate.toggleClass('show_datepicker');
+      $(this).addClass('dropbutton_open')
+      $(this).removeClass('dropbutton_close')
+    }
+  })
 
-lastdate_button.on('click', function (){
-  if(lastdate.hasClass('show_datepicker')) {
-    datepicker.hide();
-    firstdate_button.attr('disabled',false)
-    firstdate.attr('disabled',false)
-    lastdate.toggleClass('show_datepicker');
-    $(this).removeClass('dropbutton_open')
-    $(this).addClass('dropbutton_close')
-  } else {
-    datepicker.show();
-    firstdate_button.attr('disabled',true)
-    firstdate.attr('disabled',true)
-    lastdate.toggleClass('show_datepicker');
-    $(this).addClass('dropbutton_open')
-    $(this).removeClass('dropbutton_close')
+  firstdate.on('click', function(){
+    firstdate_button.trigger('click');
+    
+  })
+  lastdate.on('click', function(){
+    lastdate_button.trigger('click');
+  })
+  //button 'apply'
+  $('.datepicker').find('span[data-action="today"]').on('click',function(){
+    if(firstdate_button.hasClass('dropbutton_open')) {
+      firstdate_button.removeClass('dropbutton_open')
+      firstdate_button.addClass('dropbutton_close')
+      firstdate.removeClass('show_datepicker');
+      lastdate_button.attr('disabled',false)
+      lastdate.attr('disabled',false)
+    }
+    if(lastdate_button.hasClass('dropbutton_open')) {
+      lastdate_button.removeClass('dropbutton_open')
+      lastdate_button.addClass('dropbutton_close')
+      lastdate.removeClass('show_datepicker');
+      firstdate_button.attr('disabled',false)
+      firstdate.attr('disabled',false)
+    }
+  })
   }
-})
-//button 'apply'
-$('span[data-action="today"]').on('click',function(){
-  if(firstdate_button.hasClass('dropbutton_open')) {
-    firstdate_button.removeClass('dropbutton_open')
-    firstdate_button.addClass('dropbutton_close')
-    firstdate.removeClass('show_datepicker');
-    lastdate_button.attr('disabled',false)
-    lastdate.attr('disabled',false)
-  }
-  if(lastdate_button.hasClass('dropbutton_open')) {
-    lastdate_button.removeClass('dropbutton_open')
-    lastdate_button.addClass('dropbutton_close')
-    lastdate.removeClass('show_datepicker');
-    firstdate_button.attr('disabled',false)
-    firstdate.attr('disabled',false)
-  }
-})
+  function initSoloInput(){
+    firstdate.datepicker({
+      range: true,
+      todayButton: true,
+      clearButton: true,
+      minDate: new Date(),
+      toggleSelected: true,
+      
+    })
+  
 }
--- public/js/dropMenu.js
+++ public/js/dropMenu.js
@@ -1,15 +1,106 @@
 export function initDropMenu () {
 
-  let guestsInput = $('[name="guests"]');
-  let facilitiesInput = $('[name="facilities"]');
+  let guestsInput = $('input[name="guests"]');
+  let facilitiesInput = $('input[name="facilities"]');
 
-  function addToggleOnInput (input) {
+  addToggleOnInput(guestsInput,'guests');
+  addToggleOnInput(facilitiesInput,'facilities');
+
+  function addToggleOnInput (input,attr) {
     let inputs_button = input.next('[type="button"]'); 
     let dropMenu = input.parent().children('.dropMenu')
+    let minus_button = dropMenu.find('.minus')
+    let plus_button = dropMenu.find('.plus')
+    let span_value = dropMenu.find('span');
+    let clear_button = dropMenu.find('[data-button="clear"]');
+    let confirm_button = dropMenu.find('[data-button="confirm"]');
+    let arrOfItemsValue = span_value.map(function(){ return $(this).attr('data-startValue') });
+    (function () {
+      let i = 0;
+      if (arrOfItemsValue) {
+        span_value.each(function(){
+          if (+arrOfItemsValue[i] > 0) {$(this).siblings(minus_button).removeClass('minus-unactive')}
+           $(this).text(arrOfItemsValue[i++])
+        });
+      }
+    })() //init start value
+    
+      let counter = {
+      first : arrOfItemsValue[0],
+      second : arrOfItemsValue[1],
+      third : arrOfItemsValue[2],
+  
+      plus : function (menu_item) {
+        switch (menu_item) {
+          case '1':
+            return ++this.first
+          case '2' :
+              return ++this.second
+          case '3' :
+            return ++this.third
+          default:
+            break;
+        }
+      },
+      minus : function (menu_item) {
+        switch (menu_item) {
+          case '1':
+            if(this.first-1<0) return;
+              return --this.first
+          case '2' :
+            if(this.second-1<0) return;
+              return --this.second
+          case '3' :
+            if(this.third-1<0) return;
+              return --this.third
+          default:
+            break;
+        }
+      },
+      check : function (menu_item) {
+        if (menu_item == '1'){ 
+        return this.first}
+        if (menu_item == '2')
+        return this.second
+        if (menu_item == '3')
+        return this.third
+      },
+      reset : function () {
+        this.first = 0;
+        this.second = 0;
+        this.third = 0;
+      },
+      toString_guests: function() {
+        let sum = +this.first + +this.second
+        let modificator1 = ' гостей'
+        let modificator2 = ' младенцев'
+          if(sum%10==1 && sum !=11) modificator1 = ' гость'
+          if(sum==2 || sum==3 || sum==4) modificator1 = ' гостя'
+          if(this.third%10==1 && this.third !=11) modificator2 = ' младенец'
+          if(this.third==2 || this.third==3 || this.third==4) modificator2 = ' младенца'
+          if(this.third == 0 && sum == 0) return 'Сколько гостей'
+          if(this.third == 0) return sum+modificator1
+                             return sum+modificator1 + ', ' + this.third+modificator2
+      },
+      toString_facilities: function() {
+        let modificator1 = ' Спальня'
+        let modificator2 = ' Кровать'
+          if(this.first>1 && this.first<5) modificator1 = ' спальни'
+          if(this.first>4) modificator1 = ' спален'
+          if(this.second>1 && this.second<5) modificator2 = ' кровати'
+          if(this.second>4) modificator2 = ' кроватей'
+          if(this.first == 0 && this.second == 0) return 'Выберите удобства'
+          if(this.first != 0 && this.second == 0) return this.first + modificator1
+          if(this.second != 0 && this.first == 0) return this.second + modificator2
 
+             return this.first+modificator1 + ', ' + this.second+modificator2 + '...'
+      }
+    }
+  
     input.on('click',function(){
       inputs_button.click();
     })
+    
     inputs_button.on('click',function(){
       dropMenu.hasClass('dropMenu_show') ? dropMenu.hide() : dropMenu.show();
       dropMenu.toggleClass('dropMenu_show')
@@ -20,204 +111,47 @@ export function initDropMenu () {
           $(this).addClass('dropbutton_close')
           $(this).removeClass('dropbutton_open')
         }
-      dropMenu.hasClass('dropMenu_show') ? input.css('border-color','rgba(31, 32, 65, 0.50)') : guests.css('border-color','')
+      dropMenu.hasClass('dropMenu_show') ? input.css('border-color','rgba(31, 32, 65, 0.50)') : input.css('border-color','')
     })
-  }
-  addToggleOnInput(guestsInput);
-  addToggleOnInput(facilitiesInput);
 
-
-//----guest's counter---//
-
-  let minus_button = $('.minus')
-  let plus_button = $('.plus')
-  let clear_button = $('[data-guests-button="clear"]')
-  let confirm_button = $('[data-guests-button="confirm"]')
-  let guests_count = {
-    adult : 0,
-    children : 0,
-    baby : 0,
-
-    plus : function (guest_type) {
-      switch (guest_type) {
-        case '1':
-          return ++this.adult
-        case '2' :
-            return ++this.children
-        case '3' :
-          return ++this.baby
-        default:
-          break;
+    plus_button.on('click', function(){
+      let span = $(this).siblings('span');
+      let menu_item = $(this).parent('div').attr('data-listNumber')
+      span.html(counter.plus(menu_item))
+        if(counter.check(menu_item)!=0) {
+          $(this).siblings(minus_button).removeClass('minus-unactive')
+        }
+    })
+  
+    minus_button.on('click', function(){
+      let span = $(this).siblings('span');
+      let menu_item = $(this).parent('div').attr('data-listNumber')
+      span.html(counter.minus(menu_item))
+        if(counter.check(menu_item)==0) {
+        $(this).addClass('minus-unactive')
       }
-    },
-    minus : function (guest_type) {
-      switch (guest_type) {
-        case '1':
-          if(this.adult-1<0) return;
-            return --this.adult
-        case '2' :
-          if(this.children-1<0) return;
-            return --this.children
-        case '3' :
-          if(this.baby-1<0) return;
-            return --this.baby
-        default:
-          break;
+    })
+  
+    clear_button.on('click',function () {
+      counter.reset();
+      $(this).parent().siblings('div').children('span').text('0')
+      $(this).parent().siblings('div').children('.minus').addClass('minus-unactive')
+      if(attr == 'guests') {
+        $(this).parent().parent().siblings('input').attr('placeholder', counter.toString_guests())
       }
-    },
-    check : function (guest_type) {
-      if (guest_type == '1'){ 
-      return this.adult}
-      if (guest_type == '2')
-      return this.children
-      if (guest_type == '3')
-      return this.baby
-    },
-    reset : function () {
-      this.adult = 0;
-      this.children = 0;
-      this.baby = 0;
-    },
-    toString: function() {
-      let sum = this.adult + this.children
-      let modificator1 = ' Гостей'
-      let modificator2 = ' Младенцев'
-        if(sum%10==1 && sum !=11) modificator1 = ' Гость'
-        if(sum==2 || sum==3 || sum==4) modificator1 = ' Гостя'
-        if(this.baby%10==1 && this.baby !=11) modificator2 = ' Младенец'
-        if(this.baby==2 || this.baby==3 || this.baby==4) modificator2 = ' Младенца'
-        if(this.baby == 0 && sum == 0) return 'Сколько гостей'
-        if(this.baby == 0) return sum+modificator1
-                           return sum+modificator1 + ', ' + this.baby+modificator2
-    }
-  }
-
-  plus_button.on('click', function(){
-    let span = $(this).siblings('span');
-    let guest_type = $(this).parent('div').attr('data-listNumber')
-    span.html(guests_count.plus(guest_type))
-      if(guests_count.check(guest_type)!=0) {
-        $(this).siblings(minus_button).removeClass('minus-unactive')
+      if(attr == 'facilities') {
+        $(this).parent().parent().siblings('input').attr('placeholder', counter.toString_facilities())
       }
-  })
-
-  minus_button.on('click', function(){
-    let span = $(this).siblings('span');
-    let guest_type = $(this).parent('div').attr('data-listNumber')
-    span.html(guests_count.minus(guest_type))
-      if(guests_count.check(guest_type)==0) {
-      $(this).addClass('minus-unactive')
-    }
-  })
-
-  clear_button.on('click',function () {
-    guests_count.reset();
-    $(this).parent().siblings('div').children('span').text('0')
-    $(this).parent().siblings('div').children('.minus').addClass('minus-unactive')
-    $(this).parent().parent().siblings('input').attr('placeholder', guests_count.toString())
-  })
-
-  confirm_button.on('click',function(){
-    guests_button.click();
-    $(this).parent().parent().siblings('input').attr('placeholder', guests_count.toString())
-  })
-
-
-
-
-
-
-  // let minus_button = $('.minus')
-  // let plus_button = $('.plus')
-  // let clear_button = $('[data-guests-button="clear"]')
-  // let confirm_button = $('[data-guests-button="confirm"]')
-  // let guests_count = {
-  //   adult : 0,
-  //   children : 0,
-  //   baby : 0,
-
-  //   plus : function (guest_type) {
-  //     switch (guest_type) {
-  //       case 'adult':
-  //         return ++this.adult
-  //       case 'children' :
-  //           return ++this.children
-  //       case 'baby' :
-  //         return ++this.baby
-  //       default:
-  //         break;
-  //     }
-  //   },
-  //   minus : function (guest_type) {
-  //     switch (guest_type) {
-  //       case 'adult':
-  //         if(this.adult-1<0) return;
-  //           return --this.adult
-  //       case 'children' :
-  //         if(this.children-1<0) return;
-  //           return --this.children
-  //       case 'baby' :
-  //         if(this.baby-1<0) return;
-  //           return --this.baby
-  //       default:
-  //         break;
-  //     }
-  //   },
-  //   check : function (guest_type) {
-  //     if (guest_type == 'adult'){ 
-  //     return this.adult}
-  //     if (guest_type == 'children')
-  //     return this.children
-  //     if (guest_type == 'baby')
-  //     return this.baby
-  //   },
-  //   reset : function () {
-  //     this.adult = 0;
-  //     this.children = 0;
-  //     this.baby = 0;
-  //   },
-  //   toString: function() {
-  //     let sum = this.adult + this.children
-  //     let modificator1 = ' Гостей'
-  //     let modificator2 = ' Младенцев'
-  //       if(sum%10==1 && sum !=11) modificator1 = ' Гость'
-  //       if(sum==2 || sum==3 || sum==4) modificator1 = ' Гостя'
-  //       if(this.baby%10==1 && this.baby !=11) modificator2 = ' Младенец'
-  //       if(this.baby==2 || this.baby==3 || this.baby==4) modificator2 = ' Младенца'
-  //       if(this.baby == 0 && sum == 0) return 'Сколько гостей'
-  //       if(this.baby == 0) return sum+modificator1
-  //                          return sum+modificator1 + ', ' + this.baby+modificator2
-  //   }
-  // }
-
-  // plus_button.on('click', function(){
-  //   let span = $(this).siblings('span');
-  //   let guest_type = $(this).parent('div').attr('data-guests-type')
-  //   span.html(guests_count.plus(guest_type))
-  //     if(guests_count.check(guest_type)!=0) {
-  //       $(this).siblings(minus_button).removeClass('minus-unactive')
-  //     }
-  // })
-
-  // minus_button.on('click', function(){
-  //   let span = $(this).siblings('span');
-  //   let guest_type = $(this).parent('div').attr('data-guests-type')
-  //   span.html(guests_count.minus(guest_type))
-  //     if(guests_count.check(guest_type)==0) {
-  //     $(this).addClass('minus-unactive')
-  //   }
-  // })
-
-  // clear_button.on('click',function () {
-  //   guests_count.reset();
-  //   $(this).parent().siblings('div').children('span').text('0')
-  //   $(this).parent().siblings('div').children('.minus').addClass('minus-unactive')
-  //   $(this).parent().parent().siblings('input').attr('placeholder', guests_count.toString())
-  // })
-
-  // confirm_button.on('click',function(){
-  //   guests_button.click();
-  //   $(this).parent().parent().siblings('input').attr('placeholder', guests_count.toString())
-  // })
-
+    })
+  
+    confirm_button.on('click',function(){
+      inputs_button.click();
+      if(attr == 'guests') {
+        $(this).parent().parent().siblings('input').attr('placeholder', counter.toString_guests())
+      }
+      if(attr == 'facilities') {
+        $(this).parent().parent().siblings('input').attr('placeholder', counter.toString_facilities())
+      }
+    })
+  } 
 }
